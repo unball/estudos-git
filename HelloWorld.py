e329013c317aec9b651b7f4184d84f1d6aadfc01
RaulMyron <raul.myron@gmail.com>
2023-05-27 16:02:59 -0300

    adding helloworld.py

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+print('Hello World')
+print('signed by toropa dos macaco lokoooo ta in shock ')
+
+
+'''
+GITHUB THINGS SHOULD BE LIKE
+
+git status (vê como tá)
+
+git add * or git add --all
+
+git push (passa pro server -m 'msg')
+git push origin master -m "Your message"
+
+
+
+
+
+
+
+'''
